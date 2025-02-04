3480a6f790b17ad63863239cf09ac896c029657e
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-05 00:09:38 +0700

    enhance Nuxt.js configuration with SEO, sitemap, and image optimization features

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import viteCompression from 'vite-plugin-compression'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { definePerson } from 'nuxt-schema-org/schema'
+
 export default defineNuxtConfig({
   app: {
     head: {
+      title: 'Tefan Haetami | Software Engineer',
+      htmlAttrs: {
+        lang: 'en',
+      },
       link: [
         {
           href: process.env.NUXT_PUBLIC_BASE_URL,
@@ -54,7 +62,49 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
         },
       ],
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: 'Software Engineer and Web Developer with a passion for building digital experiences that are accessible, performant, and inclusive.',
+        },
+        {
+          property: "og:image",
+          content: "https://res.cloudinary.com/tefanhaetami/image/upload/v1738687523/WhatsApp_Image_2025-01-14_at_20.04.25_tknric.jpg",
+        },
+        {
+          property: "og:image:width",
+          content: "630"
+        },
+        {
+          property: "og:image:height",
+          content: "630",
+        },
+        {
+          property: "og:image:type",
+          content: "image/jpg",
+        },
+        
+        {
+          name: "twitter:image",
+          content: "https://res.cloudinary.com/tefanhaetami/image/upload/v1738687523/WhatsApp_Image_2025-01-14_at_20.04.25_tknric.jpg",
+        },
+        {
+          name: "twitter:image:width",
+          content: "630"
+        },
+        {
+          name: "twitter:image:height",
+          content: "630",
+        },
+        {
+          name: "twitter:image:type",
+          content: "image/jpg",
+        },
+      ]
     },
+    rootTag: 'main',
+    pageTransition: { name: 'page', mode: 'out-in' },
   },
   compatibilityDate: '2024-11-01',
   colorMode: {
@@ -71,11 +121,86 @@ export default defineNuxtConfig({
       autoInit: false,
     },
   },
-  ui: {},
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/image'],
+  hooks: {
+    'build:manifest': (manifest) => {
+      const css = manifest['node_modules/nuxt/dist/app/entry.js']?.css
+      if (css) {
+        for (let i = css.length - 1; i >= 0; i--) {
+          if (css[i].startsWith('entry')) css.splice(i, 1)
+        }
+      }
+    },
+  },
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/image', '@nuxtjs/seo', '@nuxtjs/sitemap', 'nuxt-schema-org', 'nuxt-site-config', 'nuxt-seo-utils', '@nuxtjs/robots'],
+  nitro: {
+    minify: true,
+    compressPublicAssets: {
+      brotli: true,
+    },
+  },
+  robots: {
+    blockNonSeoBots: true
+  },
   runtimeConfig: {
     public: {
       BASE_URL: process.env.NUXT_PUBLIC_BASE_URL,
     },
+  },
+  schemaOrg: {
+    identity: definePerson({
+      name: 'Tefan Haetami',
+      givenName: 'Tefan',
+      image: 'https://res.cloudinary.com/tefanhaetami/image/upload/v1738687523/WhatsApp_Image_2025-01-14_at_20.04.25_tknric.jpg',
+      description: 'Software Engineer and Web Developer with a passion for building digital experiences that are accessible, performant, and inclusive.',
+      jobTitle: 'Software Engineer, Web Developer, Lead Frontend Developer, Backend Developer, Full Stack Developer',
+      email: 'tefanhaetami87@gmail.com',
+      url: process.env.NUXT_PUBLIC_BASE_URL,
+      sameAs: [
+        'https://github.com/tefanhhh',
+        'https://www.linkedin.com/in/tefanhaetami',
+      ],
+      worksFor: {
+        '@type': 'Organization',
+        'name': 'Elemes Group',
+        'url': 'https://www.elemesgroup.com'
+      },
+    })
+  },
+  seo: {
+    redirectToCanonicalSiteUrl: true
+  },
+  sitemap: {
+    autoLastmod: true,
+    cacheMaxAgeSeconds: 3600,
+    defaults: {
+      lastmod: new Date().toISOString(),
+      priority: 1,
+      changefreq: 'weekly'
+    }
+  },
+  siteConfig: {
+    url: process.env.NUXT_PUBLIC_BASE_URL,
+    name: 'Tefan Haetami | Software Engineer',
+    description: 'Software Engineer and Web Developer with a passion for building digital experiences that are accessible, performant, and inclusive.',
+  },
+  ui: {},
+  vite: {
+    build: {
+      minify: true,
+      cssMinify: true,
+    },
+    plugins: [
+      viteCompression({
+        algorithm: 'brotliCompress',
+        threshold: 1024,
+        filter: /^(?!.*\/server\/).*\.(js|mjs|json|css|html)$/i,
+      }),
+      ViteImageOptimizer({
+        png: { quality: 80 },
+        jpeg: { quality: 80 },
+        jpg: { quality: 80 },
+        includePublic: true,
+      }),
+    ],
   },
 })
