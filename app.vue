de66af07fafcd0032619bf0d33eac6c263885f72
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-05 11:04:15 +0700

    add Google Tag Manager integration to app.vue and create gtm.js script

--- app.vue
+++ app.vue
@@ -5,5 +5,13 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-MBZPWWKX"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
   </div>
 </template>
