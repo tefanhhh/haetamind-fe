2d0a5043aae91ea993803211e6601e6adb3160f9
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-07 19:32:41 +0700

    feat: update Logo component to use webp format and change default variant to compact

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLink to="/" class="cursor-pointer">
     <NuxtImg
+      format="webp"
       :src="src[props.variant]"
       loading="lazy"
       :width="props.width"
@@ -12,17 +13,15 @@
 const props = withDefaults(
   defineProps<{
     width?: number | string
-    variant?: 'default' | 'compact' | 'soft'
+    variant?: 'compact' | 'soft'
   }>(),
   {
     width: 194,
-    variant: 'default',
+    variant: 'compact',
   },
 )
 
 const src = {
-  default:
-    'https://res.cloudinary.com/tefanhaetami/image/upload/v1732575649/Tefan_Logo_zqmroe.png',
   compact:
     'https://res.cloudinary.com/tefanhaetami/image/upload/v1733543111/Tefan_Logo_wt2qve.png',
   soft: 'https://res.cloudinary.com/tefanhaetami/image/upload/v1733543886/Tefan_Logo_Soft_hcve2e.png',
