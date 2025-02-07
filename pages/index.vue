799c2863508863da11dd322ebaa379e47ff2e10e
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-08 03:51:59 +0700

    feat: enhance UTabs component styling for improved user experience

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,30 @@
   <div class="container mx-auto px-4 sm:px-0">
     <HomeHero />
     <div class="mb-8"></div>
-    <UTabs
-      v-model="tab.value"
-      :items="tab.items"
-      :content="false"
-      :ui="{
-        list: {
-          rounded: 'rounded-full',
-          background: 'bg-primary-50',
-          height: 'h-auto',
-          tab: {
-            active: 'text-white bg-primary',
-            inactive: 'text-black',
+    <div class="max-w-[512px] mx-auto">
+      <UTabs
+        v-model="tab.value"
+        :items="tab.items"
+        :content="false"
+        :ui="{
+          list: {
             rounded: 'rounded-full',
-            height: 'h-10',
+            background: 'bg-primary-50',
+            height: 'h-auto',
+            tab: {
+              active: 'text-white bg-red-500',
+              inactive: 'text-black',
+              rounded: 'rounded-full',
+              height: 'h-10',
+            },
+            marker: {
+              rounded: 'rounded-full',
+              background: 'bg-red-500',
+            },
           },
-          marker: {
-            rounded: 'rounded-full',
-            background: 'bg-primary',
-          },
-        },
-      }"
-    />
+        }"
+      />
+    </div>
     <div class="mb-8"></div>
     <HomeSummary v-if="tab.items[tab.value].value === 'summary'" />
     <HomeWorks v-else-if="tab.items[tab.value].value === 'works'" />
