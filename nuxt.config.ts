d373b131f6168f7b117360f2b24e9bb0e42ed549
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-07 15:10:07 +0700

    add preload configuration for images in nuxt-config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import viteCompression from 'vite-plugin-compression'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { definePerson } from 'nuxt-schema-org/schema'
+import { PRELOAD } from './data/nuxt-config'
 
 export default defineNuxtConfig({
   app: {
@@ -60,6 +61,7 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
         },
+        ...PRELOAD,
       ],
       meta: [
         {
