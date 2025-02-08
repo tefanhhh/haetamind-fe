f0a6db968b476798f7b955e56afd08831c56c590
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-08 12:02:03 +0700

    feat: add SEO metadata to tools page for improved visibility

--- pages/tools.vue
+++ pages/tools.vue
@@ -3,3 +3,13 @@
     <h1 class="text-center text-xl font-bold">Still in progress...</h1>
   </div>
 </template>
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'My Tools - Tefan Haetami | Software Engineer | NodeJS | TypeScript',
+  description:
+    'This is all of my tools that I use for my daily work. I made it on purpose to help me in my work',
+  ogTitle: 'My Tools - Tefan Haetami | Software Engineer | NodeJS | TypeScript',
+  ogDescription:
+    'This is all of my tools that I use for my daily work. I made it on purpose to help me in my work',
+})
+</script>
