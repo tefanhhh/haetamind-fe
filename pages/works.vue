ac9041f0129c143d1a3b8c985efbaedeadc5f6b3
Tefan Haetami <tefanhaetami87@gmail.com>
2025-04-28 11:34:39 +0700

    feat: update project history to include React, remove unused hasher components, and adjust sitemap configuration

--- pages/works.vue
+++ pages/works.vue
@@ -9,7 +9,7 @@ const years = dayjs().diff('2018-01-01', 'year')
 useSeoMeta(
   generateSeo(
     'My Work - Tefan Haetami | Software Engineer, Node.js & TypeScript Expert',
-    `With over ${years} years of experience, I specialize in building scalable web applications and APIs using JavaScript, TypeScript, Node.js, Angular, Vue, and more. I have worked on projects ranging from small apps to enterprise-grade systems. As a consultant for Morula IVF Indonesia for 3+ years, I have played a key role in developing, maintaining, and optimizing their internal applications, ensuring high performance and reliability. Passionate about solving complex problems, improving application architecture, and delivering high-quality software solutions.`,
+    `With over ${years} years of experience, I specialize in building scalable web applications and APIs using JavaScript, TypeScript, Node.js, Angular, Vue, React, and more. I have worked on projects ranging from small apps to enterprise-grade systems. As a consultant for Morula IVF Indonesia for 3+ years, I have played a key role in developing, maintaining, and optimizing their internal applications, ensuring high performance and reliability. Passionate about solving complex problems, improving application architecture, and delivering high-quality software solutions.`,
   ),
 )
 </script>
