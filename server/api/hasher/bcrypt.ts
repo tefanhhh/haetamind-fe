b587301b01d1b7e0ad8e57e8d4990c6b68747628
Tefan Haetami <tefanhaetami87@gmail.com>
2025-04-30 11:29:45 +0700

    refactor: remove unused hasher tool components and related API endpoints

--- server/api/hasher/bcrypt.ts
+++
@@ -1,8 +0,0 @@
-import { H3Event } from 'h3'
-import { hash } from 'bcrypt'
-
-export default defineEventHandler(async (event: H3Event) => {
-  const body = await readBody(event)
-  const { value, cost } = body
-  return await hash(value, cost)
-})
